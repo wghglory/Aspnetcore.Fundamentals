ab903b9ed11a43bacbdf17ffa055fc0ec185888f
Guanghui Wang <wghglory89@gmail.com>
2017-07-18 15:56:33 +0800

    add greeter, configuration services. read appsettings.json values

---
+++ Aspnetcore.Fundamentals/Greeter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Aspnetcore.Fundamentals
+{
+
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private readonly string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["Greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
--- Aspnetcore.Fundamentals/Startup.cs
+++ Aspnetcore.Fundamentals/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,14 +13,32 @@ namespace Aspnetcore.Fundamentals
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json")
+               .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env,
+                              ILoggerFactory loggerFactory,
+                              IGreeter greeter
+                             )
         {
             loggerFactory.AddConsole();
 
@@ -30,7 +49,8 @@ namespace Aspnetcore.Fundamentals
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                string message = greeter.GetGreeting();
+                await context.Response.WriteAsync(message);
             });
         }
     }
